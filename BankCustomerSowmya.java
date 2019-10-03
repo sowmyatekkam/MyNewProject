3e531ba1cd0e3083ecbea42b98632a5cab5bad18
Sowmya <sowmyatekkam@gmail.com>
2019-10-03 18:30:18 -0400

    Initial Commit

--- BankCustomerSowmya.java
+++ BankCustomerSowmya.java
@@ -1,17 +1,1 @@
-package com.uiexample.BankProjectwithWebService;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
-public class BankCustomerSowmya {
-    @Id   
-	public String acctnum;
-    @Column
-	public String name;
-    @Column
-	public double balance;
-@Column
-public double salary;
-}
+console.log("hi");
